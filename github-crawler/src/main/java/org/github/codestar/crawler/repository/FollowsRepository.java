dbe1af02ae83240dc4c9b3bc5d56a2f9cf0af047
vitalii.levash <mr.roooot@gmail.com>
2017-07-05 18:46:05 +0300

    dao

--- github-crawler/src/main/java/org/github/codestar/crawler/repository/FollowsRepository.java
+++ github-crawler/src/main/java/org/github/codestar/crawler/repository/FollowsRepository.java
@@ -1,7 +1,12 @@
 package org.github.codestar.crawler.repository;
 
 import org.github.codestar.crawler.model.Follows;
+import org.github.codestar.crawler.model.User;
+import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.GraphRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Set;
 
 /**
  *  The {@link Follows} repository provides custom Cypher queries as repository search operations
@@ -9,4 +14,15 @@ import org.springframework.data.neo4j.repository.GraphRepository;
  *  @author vitalii.levash
  */
 public interface FollowsRepository extends GraphRepository<Follows> {
+
+    /**
+     * Efficiently batches the creation of many FOLLOWS relationships
+     * between {@link User} nodes
+     *
+     * @param follows a set of relationship entities containing a user "A" who follows a user "B"
+     */
+    @Query("FOREACH(x in {follows} | MERGE (a:User { profileId: x.userA.profileId })\n" +
+            "MERGE (b:User { profileId: x.userB.profileId })\n" +
+            "MERGE (a)-[:FOLLOWS]->(b))")
+    void saveFollows(@Param("follows") Set<Follows> follows);
 }
--- github-crawler/src/main/java/org/github/codestar/crawler/repository/UserRepository.java
+++ github-crawler/src/main/java/org/github/codestar/crawler/repository/UserRepository.java
@@ -1,10 +1,20 @@
 package org.github.codestar.crawler.repository;
 
 import org.github.codestar.crawler.model.User;
+import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.GraphRepository;
+import org.springframework.data.repository.query.Param;
 
 /**
  *  The {@link User} repository provides custom Cypher queries as repository search operations
  */
 public interface UserRepository extends GraphRepository<User> {
+
+    User findUserByLogin(String login);
+
+    @Query("MATCH (user:User { profileId: {profileId} }) RETURN id(user) as id")
+    Long getUserIdByProfileId(@Param("profileId") Long profileId);
+
+
+
 }
